8133968b63abdac26256dd8ee8e49f53e284adad
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-06-25 21:15:48 -0300

    feat(fornecedor): cria record para dto request

---
+++ src/main/java/com/api/menumaster/dtos/request/RequestFornecedorDto.java
@@ -0,0 +1,12 @@
+package com.api.menumaster.dtos.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RequestFornecedorDto(@NotBlank String razaoSocial,
+                                   @NotBlank String nomeFantasia,
+                                   @NotBlank @Size(min = 14, max = 14) String cnpj,
+                                   @NotBlank @Size(min = 9, max = 9) String inscricaoEstadual,
+                                   String endereco,
+                                   String contato) {
+}
---
+++ src/main/java/com/api/menumaster/dtos/response/ResponseFornecedorDto.java
@@ -0,0 +1,4 @@
+package com.api.menumaster.dtos.response;
+
+public record ResponseFornecedorDto() {
+}
