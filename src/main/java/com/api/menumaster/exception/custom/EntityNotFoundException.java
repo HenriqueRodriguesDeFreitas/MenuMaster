f9bb55309c2d1bac21eeb6bae69130b00a803b38
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-06-27 17:45:18 -0300

    feat(exceptions): cria EntityNotFoudException

--- src/main/java/com/api/menumaster/exception/custom/EntityNotFoundException.java
+++ src/main/java/com/api/menumaster/exception/custom/EntityNotFoundException.java
@@ -1,7 +1,7 @@
 package com.api.menumaster.exception.custom;
 
 public class EntityNotFoundException extends RuntimeException {
-  public EntityNotFoundException(String message) {
-    super(message);
-  }
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
 }
