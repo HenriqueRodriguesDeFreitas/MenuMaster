81bd23b9487078a8a7e35dc06cac9495ebe1e3ce
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-06-27 19:02:42 -0300

    feat(fornecedor) cria FornecedorRepository

--- src/main/java/com/api/menumaster/repository/FornecedorRepository.java
+++ src/main/java/com/api/menumaster/repository/FornecedorRepository.java
@@ -1,4 +1,23 @@
 package com.api.menumaster.repository;
 
-public class FornecedorRepository {
+import com.api.menumaster.model.Fornecedor;
+import org.apache.catalina.LifecycleState;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface FornecedorRepository extends JpaRepository<Fornecedor, UUID> {
+
+    Optional<Fornecedor> findByRazaoSocialIgnoreCase(String razaoSocial);
+    List<Fornecedor> findByRazaoSocialContainingIgnoreCase(String razaoSocial);
+
+    Optional<Fornecedor> findByNomeFantasiaIgnoreCase(String nomeFantasia);
+    List<Fornecedor> findByNomeFantasiaContainingIgnoreCase(String nomeFantasia);
+
+    Optional<Fornecedor> findByCnpj(String cnpj);
+    Optional<Fornecedor> findByInscricaoEstadual(String inscricaoEstadual);
+
+    List<Fornecedor> findByIsAtivoTrue();
 }
