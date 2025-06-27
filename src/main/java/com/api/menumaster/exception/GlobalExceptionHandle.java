84226f79fc05f97c636f6c8d9239ad5fd20db8b7
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-06-27 17:46:40 -0300

    fix(exceptions): add exception personalizada de NotFound

--- src/main/java/com/api/menumaster/exception/GlobalExceptionHandle.java
+++ src/main/java/com/api/menumaster/exception/GlobalExceptionHandle.java
@@ -2,6 +2,8 @@ package com.api.menumaster.exception;
 
 import com.api.menumaster.dtos.response.ResponseErroDto;
 import com.api.menumaster.exception.custom.ConflictException;
+import com.api.menumaster.exception.custom.EntityNotFoundException;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,5 +21,14 @@ public class GlobalExceptionHandle {
                 "Erro de conflito",
                 e.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ResponseErroDto> handleNotFoundException(EntityNotFoundException e) {
+        ResponseErroDto response = new ResponseErroDto(LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                "Erro de entidade não encontrada",
+                e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 }
