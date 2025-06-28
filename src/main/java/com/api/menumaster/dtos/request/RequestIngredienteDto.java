0230d9875b90286b56d8c672b694a122d3eafd02
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-06-28 17:10:35 -0300

    feat(ingrediente): cria dto record do ingrediente

--- src/main/java/com/api/menumaster/dtos/request/RequestIngredienteDto.java
+++ src/main/java/com/api/menumaster/dtos/request/RequestIngredienteDto.java
@@ -1,4 +1,19 @@
 package com.api.menumaster.dtos.request;
 
-public record RequestIngredienteDto() {
+import com.api.menumaster.model.enums.UnidadeMedida;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+
+public record RequestIngredienteDto(@NotBlank @Size(max = 30) String nome,
+                                    @Size(max = 250) String descricao,
+                                    @Digits(integer = 8, fraction = 2) BigDecimal precoCusto,
+                                    @Digits(integer = 8, fraction = 2) BigDecimal precoVenda,
+                                    @NotNull boolean isAdicional,
+                                    @NotNull UnidadeMedida unidadeMedida,
+                                    @NotNull boolean controlarEstoque
+) {
 }
