24ae536ff86eeb308f1b7b4ecf3b9e091cd78cc5
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-06-30 20:52:52 -0300

    feat(ingrediente): criação de consultas JPA no IngredienteRepository

--- src/main/java/com/api/menumaster/repository/IngredienteRepository.java
+++ src/main/java/com/api/menumaster/repository/IngredienteRepository.java
@@ -1,4 +1,35 @@
 package com.api.menumaster.repository;
 
-public interface IngredienteRepository {
+import com.api.menumaster.model.Ingrediente;
+import com.api.menumaster.model.enums.UnidadeMedida;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface IngredienteRepository extends JpaRepository<Ingrediente, UUID> {
+
+    Optional<Ingrediente> findByCodigo(Integer codigo);
+
+    Optional<Ingrediente> findByNomeIgnoreCase(String nome);
+    List<Ingrediente> findByNomeContainingIgnoreCase(String nome);
+
+    List<Ingrediente> findByDescricaoContainingIgnoreCase(String descricao);
+
+    List<Ingrediente> findByPrecoCusto(BigDecimal precoCusto);
+    List<Ingrediente> findByPrecoVenda(BigDecimal precoVenda);
+
+    List<Ingrediente> findByIsAtivoTrue();
+    List<Ingrediente> findByIsAtivoFalse();
+
+    List<Ingrediente> findByIsAdicionalTrue();
+    List<Ingrediente> findByIsAdicionalFalse();
+
+    List<Ingrediente> findByUnidadeMedida(UnidadeMedida unidadeMedida);
+
+    List<Ingrediente> findByControlarEstoqueTrue();
+    List<Ingrediente> findByControlarEstoqueFalse();
+
 }
