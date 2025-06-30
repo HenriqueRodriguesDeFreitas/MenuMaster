c552f53388e77e81c7fa45ddd7831d093a163f70
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-06-30 20:56:09 -0300

    fix(ingrediente): atualiza record ResponseIngredienteDto

--- src/main/java/com/api/menumaster/dtos/response/ResponseIngredienteDto.java
+++ src/main/java/com/api/menumaster/dtos/response/ResponseIngredienteDto.java
@@ -3,11 +3,16 @@ package com.api.menumaster.dtos.response;
 import com.api.menumaster.model.enums.UnidadeMedida;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
-public record ResponseIngredienteDto(String nome,
+public record ResponseIngredienteDto(UUID id,
+                                     Integer codigo,
+                                     String nome,
                                      String descricao,
+                                     BigDecimal estoque,
                                      BigDecimal precoCusto,
                                      BigDecimal precoVenda,
+                                     boolean isAtivo,
                                      boolean isAdicional,
                                      UnidadeMedida unidadeMedida,
                                      boolean controlarEstoque
