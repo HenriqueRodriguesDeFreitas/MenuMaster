9f7b47a8d950bc101c0b89df7111b75b6b82ca21
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-06 20:13:32 -0300

    feat: adiciona entidade de itens da entrada de ingredientes

--- src/main/java/com/api/menumaster/model/EntradaIngredienteItem.java
+++ src/main/java/com/api/menumaster/model/EntradaIngredienteItem.java
@@ -1,4 +1,75 @@
 package com.api.menumaster.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "entrada_ingrediente_itens")
 public class EntradaIngredienteItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "entrada_id")
+    private EntradaIngrediente entrada;
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "ingrediente_id")
+    private Ingrediente ingrediente;
+
+    private BigDecimal quantidade;
+    private BigDecimal custoUnitario;
+
+    public EntradaIngredienteItem(){}
+
+    public EntradaIngredienteItem(EntradaIngrediente entrada,
+                                  Ingrediente ingrediente,
+                                  BigDecimal quantidade,
+                                  BigDecimal custoUnitario) {
+        this.entrada = entrada;
+        this.ingrediente = ingrediente;
+        this.quantidade = quantidade;
+        this.custoUnitario = custoUnitario;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public EntradaIngrediente getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(EntradaIngrediente entrada) {
+        this.entrada = entrada;
+    }
+
+    public Ingrediente getIngrediente() {
+        return ingrediente;
+    }
+
+    public void setIngrediente(Ingrediente ingrediente) {
+        this.ingrediente = ingrediente;
+    }
+
+    public BigDecimal getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(BigDecimal quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getCustoUnitario() {
+        return custoUnitario;
+    }
+
+    public void setCustoUnitario(BigDecimal custoUnitario) {
+        this.custoUnitario = custoUnitario;
+    }
+
+
 }
