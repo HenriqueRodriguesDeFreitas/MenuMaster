a4499a363c885161dc39c418454510d9cfb10458
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-06 20:26:45 -0300

    feat: adiciona repository para EntradaIngrediente

--- src/main/java/com/api/menumaster/repository/EntradaIngredienteRepository.java
+++ src/main/java/com/api/menumaster/repository/EntradaIngredienteRepository.java
@@ -1,4 +1,19 @@
 package com.api.menumaster.repository;
 
-public class EntradaIngredienteRepository {
+import com.api.menumaster.model.EntradaIngrediente;
+import com.api.menumaster.model.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface EntradaIngredienteRepository extends JpaRepository<EntradaIngrediente, UUID> {
+
+    Optional<EntradaIngrediente> findByNumeroNota(String numeroNota);
+
+    List<EntradaIngrediente> findByFornecedor(Fornecedor fornecedor);
+
+    boolean existsByFornecedorIdAndNumeroNota(UUID id, String numeroNota);
 }
