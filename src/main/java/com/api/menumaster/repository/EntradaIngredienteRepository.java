5487715b0823510a282c45bac45bafa1cbcf5023
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-09 14:05:08 -0300

    .

--- src/main/java/com/api/menumaster/repository/EntradaIngredienteRepository.java
+++ src/main/java/com/api/menumaster/repository/EntradaIngredienteRepository.java
@@ -1,7 +1,6 @@
 package com.api.menumaster.repository;
 
 import com.api.menumaster.model.EntradaIngrediente;
-import com.api.menumaster.model.Fornecedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigDecimal;
