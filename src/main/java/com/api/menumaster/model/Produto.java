ebce6ce16e43a462f1afabd9b7697f24b4a34604
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-13 15:36:03 -0300

    fix: removendo imports não utilizados

--- src/main/java/com/api/menumaster/model/Produto.java
+++ src/main/java/com/api/menumaster/model/Produto.java
@@ -1,6 +1,8 @@
 package com.api.menumaster.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
