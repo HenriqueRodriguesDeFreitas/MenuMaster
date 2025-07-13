a19f6b5dca640070158f1c21202132ba4f3f303f
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-13 15:31:30 -0300

    feat: criando dto para recebimento de json para criação de produtos

--- src/main/java/com/api/menumaster/dtos/request/RequestCriaProdutoDto.java
+++ src/main/java/com/api/menumaster/dtos/request/RequestCriaProdutoDto.java
@@ -1,4 +1,13 @@
 package com.api.menumaster.dtos.request;
 
-public record RequestProdutoDto() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public record RequestCriaProdutoDto(@NotNull Long codigoProduto,
+                                    @NotBlank @Size(max = 50) String nome,
+                                    @Size(max = 250) String descricao,
+                                    @NotNull List<RequestIngredienteProdutoDto> ingredientes) {
 }
