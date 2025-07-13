c1046cef329e005f58f2833ac4a0a0735ba16580
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-13 15:33:53 -0300

    fix: removendo import não usado

--- src/main/java/com/api/menumaster/dtos/response/ResponseIngredienteProdutoDto.java
+++ src/main/java/com/api/menumaster/dtos/response/ResponseIngredienteProdutoDto.java
@@ -1,7 +1,5 @@
 package com.api.menumaster.dtos.response;
 
-import jakarta.validation.constraints.NotNull;
-
 import java.math.BigDecimal;
 
 public record ResponseIngredienteProdutoDto(String nomeIngrediente,
