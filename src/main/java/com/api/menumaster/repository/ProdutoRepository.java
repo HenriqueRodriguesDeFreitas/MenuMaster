bb2c60233905c7e3faa1872c6f2c022d25fefed8
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-13 15:47:02 -0300

    fix: criando metodos de busca

--- src/main/java/com/api/menumaster/repository/ProdutoRepository.java
+++ src/main/java/com/api/menumaster/repository/ProdutoRepository.java
@@ -1,4 +1,22 @@
 package com.api.menumaster.repository;
 
-public interface ProdutoRepository {
+import com.api.menumaster.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+    Optional<Produto> findByNomeIgnoreCase(String nome);
+    Optional<Produto> findByCodigoProduto(Long codigoProduto);
+    List<Produto> findByNomeIgnoreCaseContaining(String nome);
+    List<Produto> findByPrecoCustoBetween(BigDecimal precoInicial, BigDecimal precoFinal);
+    List<Produto> findByPrecoCusto(BigDecimal precoCusto);
+    List<Produto> findByPrecoVendaBetween(BigDecimal precoInicial, BigDecimal precoFinal);
+    List<Produto> findByPrecoVenda(BigDecimal precoVenda);
+    List<Produto> findByIsAtivoTrue();
+    List<Produto> findByIsAtivoFalse();
+
 }
