81c57447e62473f522316299ac32c0a7f2cc30b5
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-13 15:32:44 -0300

    fix: remoção das constraints

--- src/main/java/com/api/menumaster/dtos/response/ResponseProdutoDto.java
+++ src/main/java/com/api/menumaster/dtos/response/ResponseProdutoDto.java
@@ -3,8 +3,8 @@ package com.api.menumaster.dtos.response;
 import java.math.BigDecimal;
 import java.util.List;
 
-public record ResponseProdutoDto(@NotBlank String nome,
-                                 @NotNull Long codigoProduto,
+public record ResponseProdutoDto(String nome,
+                                 Long codigoProduto,
                                  String descricao,
                                  @NotNull List<RequestIngredienteProdutoDto> ingredientes) {
 }
