208005a90a13181a30621f7e8972a022e9764e77
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-13 15:42:23 -0300

    fix: mudando nome da relação com a classe ItemPedido

--- src/main/java/com/api/menumaster/model/Produto.java
+++ src/main/java/com/api/menumaster/model/Produto.java
@@ -139,7 +139,7 @@ public class Produto {
         this.unidadeMedida = unidadeMedida;
     }
 
-    public void setItensProduto(List<ItemProduto> itensProduto) {
+    public void setItensProduto(List<ItemPedido> itensProduto) {
         this.itensProduto = itensProduto;
     }
 
