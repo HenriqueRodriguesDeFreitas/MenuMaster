9bab18e52f1ba1677592e9859db2a554ed9d0951
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-18 15:46:37 -0300

    fix: adicionando import

--- src/main/java/com/api/menumaster/dtos/request/RequestCriaProdutoDto.java
+++ src/main/java/com/api/menumaster/dtos/request/RequestCriaProdutoDto.java
@@ -1,5 +1,6 @@
 package com.api.menumaster.dtos.request;
 
+import com.api.menumaster.model.enums.UnidadeMedida;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
