23ba3094b576cc132754b145d8d3d7dbe1904654
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-25 01:01:30 -0300

    feat: criando repositorio para usuario

---
+++ src/main/java/com/api/menumaster/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.api.menumaster.repository;
+
+import com.api.menumaster.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+    Optional<Usuario> findByNome(String nome);
+}
