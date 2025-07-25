f6a821b0a4af213aa8e1467bb575fbebd229dcdd
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-25 00:15:22 -0300

    fix: add metodos PreAuthorize para controle de acesso

--- src/main/java/com/api/menumaster/controller/IngredienteController.java
+++ src/main/java/com/api/menumaster/controller/IngredienteController.java
@@ -25,81 +25,97 @@ public class IngredienteController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER')")
     public ResponseEntity<?> save(@RequestBody @Valid RequestIngredienteDto dto){
         return ResponseEntity.ok(ingredienteService.save(dto));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER')")
     public ResponseEntity<?> update(@PathVariable("id")UUID id, @RequestBody @Valid RequestIngredienteUpdateDto dto){
         return ResponseEntity.ok(ingredienteService.update(id, dto));
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER', 'OPERADOR')")
     public ResponseEntity<List<?>> findAll(){
         return ResponseEntity.ok(ingredienteService.findAll());
     }
 
     @GetMapping("/byCodigo")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER', 'OPERADOR')")
     public ResponseEntity<?> findByCodigo(@RequestParam Integer codigo){
         return ResponseEntity.ok(ingredienteService.findByCodigo(codigo));
     }
 
     @GetMapping("/byNome")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER', 'OPERADOR')")
     public ResponseEntity<List<?>> findByNome(@RequestParam String nome){
         return ResponseEntity.ok(ingredienteService.findByNome(nome));
     }
 
     @GetMapping("/byDescricao")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER', 'OPERADOR')")
     public ResponseEntity<List<?>> findByDescricao(@RequestParam String descricao){
         return ResponseEntity.ok(ingredienteService.findByDescricao(descricao));
     }
 
     @GetMapping("/byPrecoCusto")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER')")
     public ResponseEntity<List<?>> findByPrecoCusto(@RequestParam BigDecimal precoCusto){
         return ResponseEntity.ok(ingredienteService.findByPrecoCusto(precoCusto));
     }
 
     @GetMapping("/byPrecoVenda")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER', 'OPERADOR')")
     public ResponseEntity<List<?>> findByPrecoVenda(@RequestParam BigDecimal precoVenda){
         return ResponseEntity.ok(ingredienteService.findByPrecoVenda(precoVenda));
     }
 
     @GetMapping("/byAtivo")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER')")
     public ResponseEntity<List<?>> findByAtivos(){
         return ResponseEntity.ok(ingredienteService.findByIsAtivo());
     }
 
     @GetMapping("/byInativo")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER')")
     public ResponseEntity<List<?>> findByNaoAtivos(){
         return ResponseEntity.ok(ingredienteService.findByIsInativo());
     }
 
     @GetMapping("/byIsAdicional")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER')")
     public ResponseEntity<List<?>> findByIsAdicional(){
         return ResponseEntity.ok(ingredienteService.findByIsAdicional());
     }
 
     @GetMapping("/byIsNotAdicional")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER')")
     public ResponseEntity<List<?>> findByNaoAdicional(){
         return ResponseEntity.ok(ingredienteService.findByIsNotAdicional());
     }
 
     @GetMapping("/byUnidadeMedida")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER', 'OPERADOR')")
     public ResponseEntity<List<?>> findByUnidadeMedia(@RequestParam UnidadeMedida unidadeMedida){
         return ResponseEntity.ok(ingredienteService.findByUnidadeMedida(unidadeMedida));
     }
 
     @GetMapping("/byControlarEstoqueIsTrue")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER')")
     public ResponseEntity<List<?>> findByControlarEstoqueIsTrue(){
         return ResponseEntity.ok(ingredienteService.findByControlarEstoqueIsTrue());
     }
 
     @GetMapping("/byControlarEstoqueIsFalse")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER')")
     public ResponseEntity<List<?>> findByControlarEstoqueIsFalse(){
         return ResponseEntity.ok(ingredienteService.findByControlarEstoqueIsFalse());
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'LIDER')")
     public ResponseEntity<Void> deleteById(@RequestParam("id") UUID id){
         ingredienteService.deleteById(id);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
