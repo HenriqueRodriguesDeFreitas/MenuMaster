cfb5a6169c864d50af831021319fc5cfc33de52f
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-25 00:10:04 -0300

    feat: criando CustomUserDetailsService

---
+++ src/main/java/com/api/menumaster/service/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.api.menumaster.service.security;
+
+import com.api.menumaster.configuration.UsuarioAutenticado;
+import com.api.menumaster.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public CustomUserDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var usuario = usuarioRepository.findByNome(username)
+                .orElseThrow(()-> new UsernameNotFoundException("Usuário não encontrado."));
+
+
+        UsuarioAutenticado usuarioAutenticado = new UsuarioAutenticado(usuario);
+        return usuarioAutenticado;
+    }
+}
