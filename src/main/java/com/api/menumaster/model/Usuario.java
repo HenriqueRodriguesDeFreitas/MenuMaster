bd090fedb95b7890ed620b24bc7d03b06cc1785d
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-25 00:01:01 -0300

    fix: removendo imports não usados mais

--- src/main/java/com/api/menumaster/model/Usuario.java
+++ src/main/java/com/api/menumaster/model/Usuario.java
@@ -1,7 +1,13 @@
 package com.api.menumaster.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
