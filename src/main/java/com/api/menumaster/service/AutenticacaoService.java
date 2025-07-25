51f19dbb4a3b9fe13d0e9a14d91c9fdc2bfb1d11
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-25 00:05:45 -0300

    feat: criando serviço para autenticação

---
+++ src/main/java/com/api/menumaster/service/AutenticacaoService.java
@@ -0,0 +1,25 @@
+package com.api.menumaster.service;
+
+import com.api.menumaster.service.security.JwtService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService {
+
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AutenticacaoService(JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public String autenticar(String usuario, String senha){
+        var auth = new UsernamePasswordAuthenticationToken(usuario, senha);
+        Authentication authentication = authenticationManager.authenticate(auth);
+        return jwtService.generateToken(authentication);
+    }
+}
