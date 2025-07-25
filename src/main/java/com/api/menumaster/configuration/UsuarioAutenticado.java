c3cc979061415cf56a4f3884f7b18eb5c0e226f5
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-25 01:00:20 -0300

    feat: criando entidade para construir o UserDetails do usuario autenticado

---
+++ src/main/java/com/api/menumaster/configuration/UsuarioAutenticado.java
@@ -0,0 +1,53 @@
+package com.api.menumaster.configuration;
+
+import com.api.menumaster.model.Usuario;
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UsuarioAutenticado implements UserDetails{
+    private final Usuario usuario;
+
+    public UsuarioAutenticado(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + usuario.getRole().getNome()));
+    }
+
+    @Override
+    public String getPassword() {
+        return usuario.getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return usuario.getNome();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
