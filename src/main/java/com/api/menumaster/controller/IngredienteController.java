9ae915d6ba79da0ee9efe398e9ea391043b35d66
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-25 00:14:22 -0300

    fix: add imports

--- src/main/java/com/api/menumaster/controller/IngredienteController.java
+++ src/main/java/com/api/menumaster/controller/IngredienteController.java
@@ -7,6 +7,7 @@ import com.api.menumaster.service.IngredienteService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
