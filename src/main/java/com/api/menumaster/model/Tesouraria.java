8ac4bf77217c3cfdf092509b634a988dc432e74e
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-26 16:14:43 -0300

    fix: adicionando ddata e usuario para reabertura, mudando o tipo LocalDate para LocalDateTime em todas as variaveis de data e adicionando meotodo para calcular saldo final

--- src/main/java/com/api/menumaster/model/Tesouraria.java
+++ src/main/java/com/api/menumaster/model/Tesouraria.java
@@ -1,9 +1,10 @@
 package com.api.menumaster.model;
 
+import com.api.menumaster.model.enums.TipoMovimento;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,8 +16,14 @@ public class Tesouraria {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "data_abertura")
-    private LocalDate dataAbertura = LocalDate.now();
+    @Column(name = "data_abertura", nullable = false)
+    private LocalDateTime dataAbertura = LocalDateTime.now();
+
+    @Column(name = "data_fechamento")
+    private LocalDateTime dataFechamento;
+
+    @Column(name = "data_reabertura")
+    private LocalDateTime dataReabertura;
 
     @Column(name = "saldo_inicial", precision = 10, scale = 2)
     private BigDecimal saldoInicial = BigDecimal.ZERO;
@@ -24,22 +31,24 @@ public class Tesouraria {
     @Column(name = "saldo_final", precision = 10, scale = 2)
     private BigDecimal saldoFinal = BigDecimal.ZERO;
 
-    @Column(name = "data_fechamento")
-    private LocalDate dataFechamento;
 
     @Column(name = "usuario_abertura", length = 20, nullable = false)
     private String usuarioAbertura;
 
-    @Column(name = "usuario_fechamento",length = 20)
+    @Column(name = "usuario_fechamento", length = 20)
     private String usuarioFechamento;
+
+    @Column(name = "usuario_reabertura", length = 20)
+    private String usuarioReabertura;
 
     @OneToMany(mappedBy = "tesouraria")
     private List<TesourariaMovimentacao> movimentacao;
 
-    public Tesouraria(){}
+    public Tesouraria() {
+    }
 
-    public Tesouraria( BigDecimal saldoInicial, String usuarioAbertura) {
-        this.dataAbertura = LocalDate.now();
+    public Tesouraria(BigDecimal saldoInicial, String usuarioAbertura) {
+        this.dataAbertura = LocalDateTime.now();
         this.saldoInicial = saldoInicial;
         this.usuarioAbertura = usuarioAbertura;
     }
@@ -48,11 +57,11 @@ public class Tesouraria {
         return id;
     }
 
-    public LocalDate getDataAbertura() {
+    public LocalDateTime getDataAbertura() {
         return dataAbertura;
     }
 
-    public void setDataAbertura(LocalDate dataAbertura) {
+    public void setDataAbertura(LocalDateTime dataAbertura) {
         this.dataAbertura = dataAbertura;
     }
 
@@ -72,11 +81,11 @@ public class Tesouraria {
         this.saldoFinal = saldoFinal;
     }
 
-    public LocalDate getDataFechamento() {
+    public LocalDateTime getDataFechamento() {
         return dataFechamento;
     }
 
-    public void setDataFechamento(LocalDate dataFechamento) {
+    public void setDataFechamento(LocalDateTime dataFechamento) {
         this.dataFechamento = dataFechamento;
     }
 
@@ -103,4 +112,39 @@ public class Tesouraria {
     public void setMovimentacao(List<TesourariaMovimentacao> movimentacao) {
         this.movimentacao = movimentacao;
     }
+
+    public String getUsuarioReabertura() {
+        return usuarioReabertura;
+    }
+
+    public void setUsuarioReabertura(String usuarioReabertura) {
+        this.usuarioReabertura = usuarioReabertura;
+    }
+
+    public LocalDateTime getDataReabertura() {
+        return dataReabertura;
+    }
+
+    public void setDataReabertura(LocalDateTime dataReabertura) {
+        this.dataReabertura = dataReabertura;
+    }
+
+    public void calcularSaldoFinal() {
+        if (movimentacao == null || movimentacao.isEmpty()) {
+            this.saldoFinal = this.saldoInicial;
+            return;
+        }
+        BigDecimal totalEntrada = movimentacao.stream()
+                .filter(t -> TipoMovimento.ENTRADA.equals(t.getTipoMovimento()))
+                .map(TesourariaMovimentacao::getValor)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        BigDecimal totalSaida = movimentacao.stream()
+                .filter(t -> TipoMovimento.SAIDA.equals(t.getTipoMovimento()))
+                .map(TesourariaMovimentacao::getValor)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        this.saldoFinal = this.saldoInicial.add(totalEntrada).subtract(totalSaida);
+    }
+
 }
