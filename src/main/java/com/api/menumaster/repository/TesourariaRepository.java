4e62608367e2c1b0343f6468d23a2cdbd89768ca
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-26 16:16:50 -0300

    fix: add outros metodos de pesquisa

--- src/main/java/com/api/menumaster/repository/TesourariaRepository.java
+++ src/main/java/com/api/menumaster/repository/TesourariaRepository.java
@@ -3,11 +3,21 @@ package com.api.menumaster.repository;
 import com.api.menumaster.model.Tesouraria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface TesourariaRepository extends JpaRepository<Tesouraria, UUID> {
 
-  boolean existsByDataFechamentoIsNull();
-  Optional<Tesouraria> findFirstByDataFechamentoIsNotNullOrderByDataFechamentoDesc();
+    boolean existsByDataFechamentoIsNull();
+
+    Optional<Tesouraria> findFirstByDataFechamentoIsNotNullOrderByDataFechamentoDesc();
+
+    Optional<Tesouraria> findByDataFechamentoIsNull();
+
+    Optional<Tesouraria> findByDataFechamento(LocalDateTime hoje);
+
+    List<Tesouraria> findByDataFechamentoBetween(LocalDateTime inicio, LocalDateTime fim);
 }
