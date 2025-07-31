8680cdef7a4a59f40b5dd261463284a35dcf4537
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-31 17:42:40 -0300

    feat: mudando referencias a role para authority

--- src/main/java/com/api/menumaster/service/UsuarioService.java
+++ src/main/java/com/api/menumaster/service/UsuarioService.java
@@ -4,26 +4,29 @@ import com.api.menumaster.dtos.request.RequestUsuarioDto;
 import com.api.menumaster.dtos.response.ResponseUsuarioDto;
 import com.api.menumaster.exception.custom.ConflictEntityException;
 import com.api.menumaster.exception.custom.EntityNotFoundException;
-import com.api.menumaster.model.Role;
+import com.api.menumaster.model.Authority;
 import com.api.menumaster.model.Usuario;
-import com.api.menumaster.repository.RoleRepository;
+import com.api.menumaster.repository.AuthorityRepository;
 import com.api.menumaster.repository.UsuarioRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
-    private final RoleRepository roleRepository;
+    private final AuthorityRepository authorityRepository;
     private final PasswordEncoder encoder;
 
-    public UsuarioService(UsuarioRepository usuarioRepository, RoleRepository roleRepository,
-                          PasswordEncoder encoder) {
+    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder encoder,
+                          AuthorityRepository authorityRepository) {
         this.usuarioRepository = usuarioRepository;
-        this.roleRepository = roleRepository;
         this.encoder = encoder;
+        this.authorityRepository = authorityRepository;
     }
 
     @Transactional
@@ -33,15 +36,20 @@ public class UsuarioService {
                     throw new ConflictEntityException("Usuário já cadastrado");
                 });
 
-        Role role = roleRepository.findByNome(dto.role())
+        Authority authority = authorityRepository.findByNome(dto.authority())
                 .orElseThrow(() -> new EntityNotFoundException("Permissão não encontrada."));
 
-        Usuario usuario = new Usuario(dto.nome(), encoder.encode(dto.senha()), role);
+        Usuario usuario = new Usuario();
+        usuario.setNome(dto.nome());
+        usuario.setSenha(encoder.encode(dto.senha()));
+        usuario.adicionarAuthority(authority);
 
         return converterUsuarioParaResponseDto(usuarioRepository.save(usuario));
     }
 
     private ResponseUsuarioDto converterUsuarioParaResponseDto(Usuario usuario) {
-        return new ResponseUsuarioDto(usuario.getId(), usuario.getNome(), usuario.getSenha(), usuario.getRole().getNome());
+        Set<String> authorities = usuario.getAuthorities().stream()
+                .map(a -> a.getAuthority().getNome()).collect(Collectors.toSet());
+        return new ResponseUsuarioDto(usuario.getId(), usuario.getNome(), usuario.getSenha(), authorities);
     }
 }
