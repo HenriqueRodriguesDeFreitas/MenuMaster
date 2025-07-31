7890c9c97cd11d90afbeaa85abd713cf638f4e3a
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-07-31 17:18:11 -0300

    fix: criando dto para requisição de movimentos para tesouraria

---
+++ src/main/java/com/api/menumaster/dtos/request/RequestMovimentoTesourariaDto.java
@@ -0,0 +1,15 @@
+package com.api.menumaster.dtos.request;
+
+import com.api.menumaster.model.enums.FormaPagamento;
+import com.api.menumaster.model.enums.TipoMovimento;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+
+public record RequestMovimentoTesourariaDto(@NotNull TipoMovimento tipoMovimento,
+                                            @NotNull FormaPagamento formaPagamento,
+                                            @NotNull @Digits(integer = 10, fraction = 2) BigDecimal valor,
+                                            @NotNull @Size(max = 200) String descricao) {
+}
