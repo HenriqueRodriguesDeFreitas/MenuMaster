3b0f5cd6670729fa6b06aa2a017c5e80c7cb82d3
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-08-17 19:06:10 -0300

    fix: add routes for find by username, find all, update and delete.

--- src/main/java/com/api/menumaster/controller/UsuarioController.java
+++ src/main/java/com/api/menumaster/controller/UsuarioController.java
@@ -1,15 +1,18 @@
 package com.api.menumaster.controller;
 
+import com.api.menumaster.dtos.request.RequestUserUpdateDto;
 import com.api.menumaster.dtos.request.RequestUsuarioDto;
+import com.api.menumaster.dtos.response.ResponseUsuarioDto;
 import com.api.menumaster.service.UsuarioService;
 import jakarta.annotation.security.PermitAll;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuario")
@@ -22,8 +25,36 @@ public class UsuarioController {
     }
 
     @PostMapping
-    @PreAuthorize("asAuthority('USUARIO_CREATE')")
-    public ResponseEntity<?> salvar(@RequestBody @Valid RequestUsuarioDto dto){
+    @PreAuthorize("hasAnyAuthority('ADMIN','USUARIO_CREATE')")
+    public ResponseEntity<?> salvar(@RequestBody @Valid RequestUsuarioDto dto) {
         return ResponseEntity.ok(usuarioService.salvar(dto));
+    }
+
+    @PutMapping("/{idUsuario}")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'USUARIO_EDIT')")
+    public ResponseEntity<ResponseUsuarioDto> updateUser(@PathVariable(value = "idUsuario",
+                                                                 required = true) UUID idUsuario,
+                                                         @RequestBody @Valid RequestUserUpdateDto dto) {
+        return ResponseEntity.ok(usuarioService.updateUser(idUsuario, dto));
+    }
+
+    @DeleteMapping("/{idUsuario}")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'USUARIO_DELETE')")
+    public ResponseEntity<Void> deleteUser(@PathVariable(value = "idUsuario", required = true)
+                                           UUID idUsuario) {
+        usuarioService.deleteUser(idUsuario);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'USUARIO_READ')")
+    public ResponseEntity<List<ResponseUsuarioDto>> findAllUsers(){
+        return ResponseEntity.ok(usuarioService.findAllUsers());
+    }
+
+    @GetMapping("/byName/{name}")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'USUARIO_READ')")
+    public ResponseEntity<List<ResponseUsuarioDto>> findUsersByName(@PathVariable("name") String name){
+        return ResponseEntity.ok(usuarioService.findUsersByName(name));
     }
 }
