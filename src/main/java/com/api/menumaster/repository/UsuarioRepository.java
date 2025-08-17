c68ac512cd2db18a95df98ae1bad3703111ad0a6
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-08-17 18:58:35 -0300

    fix: add metod to find by name

--- src/main/java/com/api/menumaster/repository/UsuarioRepository.java
+++ src/main/java/com/api/menumaster/repository/UsuarioRepository.java
@@ -3,9 +3,11 @@ package com.api.menumaster.repository;
 import com.api.menumaster.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
     Optional<Usuario> findByNome(String nome);
+    List<Usuario> findByNomeContainingOrderByNome(String nome);
 }
