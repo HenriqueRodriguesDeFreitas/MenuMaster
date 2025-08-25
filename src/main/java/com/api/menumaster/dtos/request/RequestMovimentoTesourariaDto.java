b9607301c8e31778df924e466c94579c249c5f9a
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-08-25 17:44:05 -0300

    fix: removendo variavel para tipo de movimento

--- src/main/java/com/api/menumaster/dtos/request/RequestMovimentoTesourariaDto.java
+++ src/main/java/com/api/menumaster/dtos/request/RequestMovimentoTesourariaDto.java
@@ -8,8 +8,8 @@ import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
-public record RequestMovimentoTesourariaDto(@NotNull TipoMovimento tipoMovimento,
-                                            @NotNull FormaPagamento formaPagamento,
-                                            @NotNull @Digits(integer = 10, fraction = 2) BigDecimal valor,
-                                            @NotNull @Size(max = 200) String descricao) {
+public record RequestMovimentoTesourariaDto(
+        @NotNull FormaPagamento formaPagamento,
+        @NotNull @Digits(integer = 10, fraction = 2) BigDecimal valor,
+        @NotNull @Size(max = 200) String descricao) {
 }
