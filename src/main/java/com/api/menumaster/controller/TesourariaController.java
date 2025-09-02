aee0d65492dbed44adbd5320c61dc97bde95bc97
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-09-02 15:00:40 -0300

    fix: adicionando PreAuthorize para autorização

--- src/main/java/com/api/menumaster/controller/TesourariaController.java
+++ src/main/java/com/api/menumaster/controller/TesourariaController.java
@@ -3,6 +3,7 @@ package com.api.menumaster.controller;
 import com.api.menumaster.dtos.response.ResponseTesourariaDto;
 import com.api.menumaster.service.TesourariaService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -21,34 +22,39 @@ public class TesourariaController {
     }
 
     @PostMapping("/abrir")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'TESOURARIA_ABRIR')")
     public ResponseEntity<ResponseTesourariaDto> abrirTesouraria() {
         return ResponseEntity.ok(tesourariaService.abrirTesouraria(getAuthentication()));
     }
 
     @PostMapping("/fechar")
-    public ResponseEntity<ResponseTesourariaDto> fecharTesouraria(){
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'TESOURARIA_FECHAR')")
+    public ResponseEntity<ResponseTesourariaDto> fecharTesouraria() {
         return ResponseEntity.ok(tesourariaService.fecharTesouraria(getAuthentication()));
     }
 
     @PostMapping("/reabrir")
-    public ResponseEntity<ResponseTesourariaDto> reabrirTesouraria(){
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'TESOURARIA_ABRIR')")
+    public ResponseEntity<ResponseTesourariaDto> reabrirTesouraria() {
         return ResponseEntity.ok(tesourariaService.reabrirTesouraria(getAuthentication()));
     }
 
     @GetMapping
-    public ResponseEntity<List<ResponseTesourariaDto>> buscarTodasTesourarias(){
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'TESOURARIA_READ')")
+    public ResponseEntity<List<ResponseTesourariaDto>> buscarTodasTesourarias() {
         return ResponseEntity.ok(tesourariaService.buscarTodasTesourarias());
     }
 
     @GetMapping("/byDataAbertura")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'TESOURARIA_READ')")
     public ResponseEntity<List<?>> buscarPorDataAbertura(
-            @RequestParam(value = "dataInicial", required = true)LocalDate dataInicio,
-            @RequestParam(value = "dataFinal", required = false)LocalDate dataFinal){
-        return ResponseEntity.ok(tesourariaService.buscarTesourariasPorDataAbertura(dataInicio,dataFinal));
+            @RequestParam(value = "dataInicial", required = true) LocalDate dataInicio,
+            @RequestParam(value = "dataFinal", required = false) LocalDate dataFinal) {
+        return ResponseEntity.ok(tesourariaService.buscarTesourariasPorDataAbertura(dataInicio, dataFinal));
     }
 
 
-    private Authentication getAuthentication(){
+    private Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 }
