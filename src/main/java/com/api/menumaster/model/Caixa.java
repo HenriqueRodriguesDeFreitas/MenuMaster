706f882c6d72e5a2d6e2808921fa534578ad60fb
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-09-07 21:21:19 -0300

    fix: add metodo set para Id

--- src/main/java/com/api/menumaster/model/Caixa.java
+++ src/main/java/com/api/menumaster/model/Caixa.java
@@ -56,6 +56,10 @@ public class Caixa {
         return id;
     }
 
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
     public BigDecimal getSaldoInicial() {
         return saldoInicial;
     }
