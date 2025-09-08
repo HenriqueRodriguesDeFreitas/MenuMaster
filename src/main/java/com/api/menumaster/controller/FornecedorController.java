f468a6849bb224bb13ee9e932c294b6de688cc10
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-09-08 20:09:47 -0300

    fix: addd PreAuthorize

--- src/main/java/com/api/menumaster/controller/FornecedorController.java
+++ src/main/java/com/api/menumaster/controller/FornecedorController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Size;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,47 +24,55 @@ public class FornecedorController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'FORNECEDOR_CREATE')")
     public ResponseEntity<?> save(@RequestBody @Valid RequestFornecedorDto dto) {
-        return ResponseEntity.ok(fornecedorService.save(dto));
+        return ResponseEntity.ok(fornecedorService.salvarNovoFornecedor(dto));
     }
 
     @GetMapping("/byCnpj")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'FORNECEDOR_READ')")
     public ResponseEntity<?> findByCnpj(@RequestParam("cnpj")
                                         @Size(min = 14, max = 14) String cnpj) {
-        return ResponseEntity.ok(fornecedorService.findByCnpj(cnpj));
+        return ResponseEntity.ok(fornecedorService.buscarFornecedorPorCnpj(cnpj));
     }
 
     @GetMapping("/byInscricaoEstadual")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'FORNECEDOR_READ')")
     public ResponseEntity<?> findByInscricaoEstadual(@RequestParam("inscricaoEstadual")
                                                      @Size(min = 9, max = 9)
                                                      String inscricaoEstadual) {
-        return ResponseEntity.ok(fornecedorService.findByIncricaoEstadual(inscricaoEstadual));
+        return ResponseEntity.ok(fornecedorService.buscarFornecedorPorInscricaoEstadual(inscricaoEstadual));
     }
 
     @GetMapping("/byNomeFantasia")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'FORNECEDOR_READ')")
     public ResponseEntity<List<?>> findByNomeFantasia(@RequestParam("nomeFantasia")
                                                       String nomeFantasia) {
-        return ResponseEntity.ok(fornecedorService.findByNomeFantasia(nomeFantasia));
+        return ResponseEntity.ok(fornecedorService.buscarFornecedorPorNomeFantasia(nomeFantasia));
     }
 
     @GetMapping("/byRazaoSocial")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'FORNECEDOR_READ')")
     public ResponseEntity<List<?>> findByRazaoSocial(@RequestParam("razaoSocial")
                                                      String razaoSocial) {
-        return ResponseEntity.ok(fornecedorService.findByRazaoSocial(razaoSocial));
+        return ResponseEntity.ok(fornecedorService.buscarFornecedorPorRazaoSocial(razaoSocial));
     }
 
     @GetMapping("/byFornecedoresAtivos")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'FORNECEDOR_READ')")
     public ResponseEntity<List<?>> findByFornecedoresAtivos() {
-        return ResponseEntity.ok(fornecedorService.findByFornecedoresAtivos());
+        return ResponseEntity.ok(fornecedorService.buscarFornecedorPorFornecedoresAtivos());
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'FORNECEDOR_UPDATE')")
     public ResponseEntity<?> update(@PathVariable("id") UUID id,
                                     @RequestBody @Valid RequestFornecedorUpdateDto dto) {
-        return ResponseEntity.ok(fornecedorService.update(id, dto));
+        return ResponseEntity.ok(fornecedorService.atualizarFornecedor(id, dto));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'FORNECEDOR_DELETE')")
     public ResponseEntity<Void> deleteById(@PathVariable("id") UUID id){
         fornecedorService.deleteById(id);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
